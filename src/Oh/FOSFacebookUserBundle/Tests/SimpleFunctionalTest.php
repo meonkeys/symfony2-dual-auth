09a7f249dede00b4815411baf158083cdbe231dc
Adam Monsen <haircut@gmail.com>
2012-09-23 16:55:34 -0700

    Add failing functional test.
    I'm trying to repro a problem I'm seeing with:
    
    * symfony 2.0
    * FOSUserBundle + FOSFacebookBundle
    * functional tests
    
    See:
    
    * https://groups.google.com/d/msg/symfony2/oKFOT3L-R-0/gSx_6mWuMKEJ
    
    Hungry for more? See:
    
    * http://stackoverflow.com/questions/12557290/symfony-2-1-phpunit-c-app-tries-to-run-symfony-core-tests
    * http://stackoverflow.com/questions/9046669/fosfacebookbundle-and-fosuserbundle
    * http://stackoverflow.com/questions/8900636/fosfacebookbundle-and-fosuserbundle-working-together
    * http://stackoverflow.com/questions/10862488/cant-get-custom-user-provider-working-with-fosfacebookbundle-and-fosuserbundle
    * http://stackoverflow.com/questions/12162394/fosfacebookbundle-breaks-remember-me-of-fosuserbundle
    * https://groups.google.com/d/topic/symfony2/oKFOT3L-R-0/discussion

---
+++ src/Oh/FOSFacebookUserBundle/Tests/SimpleFunctionalTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Oh\FOSFacebookUserBundle\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Tests\Functional\WebTestCase;
+
+class SimpleFunctionTest extends WebTestCase {
+
+  public function testIndex() {
+    $client = static::createClient();
+
+    $crawler = $client->request('GET', '/login');
+
+    $this->assertEquals(1, $crawler->filter('html:contains("Remember me")')->count());
+  }
+
+}
